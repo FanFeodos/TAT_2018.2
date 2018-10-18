13a4bdab6ab8ddab65f2d6f453c50218b5cc4902
Igor Yakushev <44230061+FanFeodos@users.noreply.github.com>
2018-10-18 12:42:04 +0300

    task1 v2

---
+++ SplitToClasses/task_DEV-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Algorithms;
+
+namespace task_DEV_1
+{
+    class Program
+    {
+        // Entry points
+        static void Main(string[] args)
+        {
+            // Getting input
+            string inputtedString = StringOperations.ConcatenationInput(args);
+            // Output to console
+            Console.WriteLine(args.Length > 0 ? "String: " + inputtedString : "String is empty");
+            Console.WriteLine("String length: " + inputtedString.Length);
+            Console.WriteLine("Substring with unique symbols max length: " + StringOperations.MaxSubstringLength(inputtedString));
+        }        
+    }
+}

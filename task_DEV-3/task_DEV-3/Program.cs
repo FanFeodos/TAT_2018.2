f45747f813f843ffa1485b7e3f4c823384857ba3
Igor Yakushev <44230061+FanFeodos@users.noreply.github.com>
2018-10-25 19:15:12 +0300

    Final 3 task

---
+++ task_DEV-3/task_DEV-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_DEV_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StringBuilder output = new StringBuilder();
+                ConvertToSystems outputted = new ConvertToSystems();
+                output = outputted.Convertation(args);
+                Console.WriteLine(output);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
